74c78e6f812d0221c6ea7a6f970799c6e38703ab
sitso-en <sitso.nkrumah@gmail.com>
2026-01-22 21:42:12 +0000

    First time doing java, i like it yet

---
+++ Factorial.java
@@ -0,0 +1,29 @@
+public class Factorial{
+    public static void main(String[] args){
+        int roundedStd =(int) Math.round(standardDeviation());
+        int result = factorial(roundedStd);
+        System.out.println("Factorial: " + result);
+    }
+
+    public static double average(){
+        int sum =0;
+        for (int i=1; i <=10; i++){
+            sum+=i;
+        }
+        double average = sum /10;
+        return average;
+    }
+
+    public static double sumofSquaredDifferences(){
+        double average = average();
+
+        double squaredDiff =0;
+
+        for(int n=1; n<=10;n++){
+            squaredDiff+= Math.pow((n-average), 2);
+        }
+        return squaredDiff;
+    }
+
+    
+
