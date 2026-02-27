ec7214f5408e0276dabd7eae78341bed0a610a77
Enoch Sitsofe <sitso.nkrumah@gmail.com>
2026-02-27 23:37:47 +0000

    Fix FizzBuzz condition order and logic
    Refactored FizzBuzz logic to improve readability.

--- tutorials/Buzz.java
+++ tutorials/Buzz.java
@@ -12,12 +12,13 @@ public class Buzz {
         if(number % 5==0 && number %3==0){
             System.out.println("FizzBuzz");
         }
+        else if(number % 5==0){
+            System.out.println("Fizz");
+        }
         else if(number%3==0){
             System.out.println("Buzz");
         }
-        else if(number % 5==0){
-            System.out.println("Fizz");
-        }else{
+        else{
             System.out.println(number);
         }
 
