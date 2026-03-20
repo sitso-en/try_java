a8f706e218d1fe1bd7dd17a12de1cc446306e9bf
sitso-en <sitso.nkrumah@gmail.com>
2026-03-20 15:07:34 +0000

    Got it to working

--- tutorials/Matrices.java
+++ tutorials/Matrices.java
@@ -15,6 +15,71 @@ public class Matrices {
         for(int i=0; i<k.length; i++){
                 product *= k[i][i] * j[i][i];
             }
-        System.out.println(product);
+        System.out.println("Q1: " + product);
+
+        int trailJ = 1;
+        int trailK = 1;
+        for(int i=0; i<k.length; i++){
+            trailJ *= j[i][j.length - 1 - i];
+            trailK *= k[i][k.length - 1 - i];
+        }
+        System.out.println("Q2: " + (trailJ - trailK));
+
+        for(int i=0; i<k.length; i++){
+            for(int m=0; m<k[i].length; m++){
+                int temp = k[i][m];
+                k[i][m] = j[i][m];
+                j[i][m] = temp;
+            }
+        }
+        System.out.println("Q3: K and J swapped");
+        System.out.print("K: ");
+        for(int i=0; i<k.length; i++){
+            for(int m=0; m<k[i].length; m++){
+                System.out.print(k[i][m] + " ");
+            }
+            System.out.print("| ");
+        }
+        System.out.println();
+
+        System.out.print("J: ");
+        for(int i=0; i<j.length; i++){
+            for(int m=0; m<j[i].length; m++){
+                System.out.print(j[i][m] + " ");
+            }
+            System.out.print("| ");
+        }
+        System.out.println();
+
+        for(int i=0; i<k.length; i++){
+            for(int m=0; m<k[i].length; m++){
+                int temp = k[i][m];
+                k[i][m] = j[i][m];
+                j[i][m] = temp;
+            }
+        }
+
+        int leadK = 1;
+        int trailK2 = 1;
+        for(int i=0; i<k.length; i++){
+            leadK *= k[i][i];
+            trailK2 *= k[i][k.length - 1 - i];
+        }
+        System.out.println("Q4: " + (leadK - trailK2));
+
+        double sumK = 0, sumJ = 0;
+        int count = 0;
+        for(int i=0; i<k.length; i++){
+            for(int m=0; m<k[i].length; m++){
+                sumK += k[i][m];
+                sumJ += j[i][m];
+                count++;
+            }
+        }
+        
+        double avgK = sumK / count;
+        double avgJ = sumJ / count;
+        double mean = (avgK + avgJ) / 2;
+        System.out.println("Q5: " + mean);
     }
 }
